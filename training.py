dd76801fe1555e7ab548d06fa4c8f868666bad08
NIRVANA-CHEL <name@gmail.com>
2022-08-24 23:56:50 +0300

    Change text in text-file

---
+++ training.py
@@ -0,0 +1,16 @@
+# d = input('Введите текст: ')
+
+f = open('VIRUS.txt', 'r')
+# f.write(d + '\n')
+# # print(f.read())
+for h in f:
+    print(h, end="")
+
+
+f.close()
+# for i in l:
+#     f.write(index + '\n')
+
+
+# print('What a wonderfull world!')
+
